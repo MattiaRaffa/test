39f2fff1d1ee56676f954cb79bb0b13afe948311
Mattia <mattia.raffa@gmail.com>
2020-03-22 22:14:53 +0100

    added angelo function e has to be set!!

--- R2 fit.py
+++ R2 fit.py
@@ -1,14 +1,17 @@
 import numpy as np
 from scipy.optimize import minimize
 
+K=6
+L=5
 def rosen(x):
-    """The Rosenbrock function"""
-    return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
+    #"""The Rosenbrock function"""
+    #return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
+    """The Romeo function"""
+    return sum (((K*L*e**(-K*x-x0))/(1+e**(-K*x-x0))**2)**2 - x**2)
 
 x0 = np.array([1.3, 0.7, 0.8, 1.9, 1.2])
 res = minimize(rosen, x0, method='nelder-mead',
                options={'xatol': 1e-8, 'disp': True})
 
 print(res.x)
-[1. 1. 1. 1. 1.]
 
